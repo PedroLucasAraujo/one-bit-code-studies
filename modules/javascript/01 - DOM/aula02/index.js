0b8a09f65e82980a815f4d3f28ef21683c144eba
Pedro Lucas Pinheiro Araujo <pedrolucas.araujo@agenciam3.com>
2023-12-30 18:32:51 -0300

    feat: introduction to events in html

---
+++ modules/javascript/01 - DOM/aula02/index.js
@@ -0,0 +1,15 @@
+function onMouseEnter() {
+  console.log("Entrada do Mouse");
+}
+function onChange() {
+  console.log("Mudança de Valor");
+}
+function onFocus() {
+  console.log("Entrada de Foco");
+}
+function onBlur() {
+  console.log("Saída de Foco");
+}
+function onClick() {
+  console.log("Clique realizado!");
+}
